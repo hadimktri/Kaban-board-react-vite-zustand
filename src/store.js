29d69ba6b09e10dbd82848800be9c9dc75440d94
Hadi <hadimktri@gmail.comygit config --global user.email hadimktri@gmail.com>
2023-04-29 09:57:41 -0700

    devTools

--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
 import { create } from "zustand";
-
+import { devtools, persist } from "zustand/middleware"
 const store = (set) => ({
     tasks: [],
     dragged: null,
-    addTask: (title, state) => set(store => ({ tasks: [...store.tasks, { title, state }] })),
-    deleteTask: (title) => set(store => ({ tasks: store.tasks.filter(task => task.title !== title) })),
-    setDragged: (title) => set({ dragged: title }),
-    dropTask: (title, state) => set(store => ({ tasks: store.tasks.map(task => task.title === title ? { title, state } : task) })),
+    addTask: (title, state) => set(store => ({ tasks: [...store.tasks, { title, state }] }), false, 'addTask'),
+    deleteTask: (title) => set(store => ({ tasks: store.tasks.filter(task => task.title !== title) }), false, 'deleteTask'),
+    setDragged: (title) => set({ dragged: title }, false, 'setDragged'),
+    dropTask: (title, state) => set(store => ({ tasks: store.tasks.map(task => task.title === title ? { title, state } : task) }), false, 'dropTask'),
 })
 
-export const useStore = create(store)
-
-
-
+export const useStore = create(persist(devtools(store), { name: 'store' }))
